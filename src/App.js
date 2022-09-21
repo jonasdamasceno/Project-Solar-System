d9067d2ef41a5c9a5b330bd8c02c1c8d976947c5
JonasDamasceno <jonasmichael2021@outlook.com>
2022-09-21 18:27:47 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Header from './components/Header';
+// import Missions from './components/Missions';
+// import SolarSystem from './components/SolarSystem';
+// import MissionCard from './components/MissionCard';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <section className="container">
+        <Header />
+        {/* <SolarSystem />
+        <Missions />
+        <MissionCard /> */}
+      </section>
+    );
   }
 }
 
